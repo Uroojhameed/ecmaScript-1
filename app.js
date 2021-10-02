e2fc2b9d4fefd5a79d584549ce0d822ba9a56b2e
Uroojhameed <81069857+Uroojhameed@users.noreply.github.com>
2021-10-02 20:14:16 +0500

    Add files via upload
    assignment has done

---
+++ app.js
@@ -0,0 +1,55 @@
+
+// assignment (02)
+
+//write a program which shows the couter after each second.
+
+
+// setTimeout(() => {
+//     console.log('1')
+// }, 3000);
+
+
+// write a function which takes your name and displays the greeting with your name
+
+// function name() {
+//     alert("hi i'm urooj" )
+// }
+// name();
+
+
+// write a function(arrow function) which takes two values and return its sum as a result
+
+// var a =()=>{
+//     return 2*4
+// }
+// console.log(a())
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+
+// let userNum =+prompt("write your number" )
+// var b=()=>{
+//     return userNum*0.5
+    
+// }
+// document.write(b())
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// var abc=[1,2,3,4,5]
+// abc.map((val)=>{
+//    return console.log(val)
+// })
+
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+
+// const myStudent = [
+//        { id: 1, name: "aaa", age: 15, fees:'yes' },
+//        { id: 2, name: "bbb", age: 16, fees:`yes`},
+//        { id: 3, name: "ccc", age: 18, fees:`no`},
+//        { id: 4, name: "ddd", age: 22, fees:`yes`},
+//        { id: 5, name: "zzz", age: 12, fees:`no`},
+//        { id: 6, name: "yyy", age: 12, fees:`no`},
+//    ]
+// let student = myStudent.map((a)=> console.log(`name ${a.name} id ${a.id}`))
